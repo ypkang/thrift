d5a0f26b63baaca0f32599986265967b45f40422
Yiping Kang <ypkang@umich.edu>
2015-10-24 22:18:19 -0400

    add changes to make thrift to work with torch/luajit2.1

--- test/lua/test_basic_server.lua
+++ test/lua/test_basic_server.lua
@@ -14,6 +14,7 @@
 -- KIND, either express or implied. See the License for the                                                                                                                                                                           
 -- specific language governing permissions and limitations                                                                                                                                                                            
 -- under the License.                                                                                                                                                                                                                 
+package.path = package.path .. ";../gen-lua/?.lua"
 
 require('ThriftTest_ThriftTest')
 require('TSocket')
